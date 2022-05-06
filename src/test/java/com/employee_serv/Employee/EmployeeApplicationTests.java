b1a460c05a34b9ab114646dd0b5eea718964761f
Shivanandghule <shivanandghule24@gmail.com>
2022-05-06 10:20:47 +0530

    first commit

---
+++ src/test/java/com/employee_serv/Employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_serv.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
